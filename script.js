cb5d50fd00266efe217a92e9f7fc47e900af2bde
Rey  Baterbonia <poochinng@gmail.com>
2022-09-29 13:28:55 +0800

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var days = new Date().getDay();
+var hours = new Date().getHours();
+var minutes = new Date().getMinutes();
+var seconds = new Date().getSeconds();
+
+
+document.getElementById('days').innerHTML = days;
+document.getElementById('hours').innerHTML = hours;
+document.getElementById('minutes').innerHTML = minutes;
+document.getElementById('seconds').innerHTML = seconds;
