3edebc217c44b70005510dfb7f2f9e7608420e0b
Rey  Baterbonia <poochinng@gmail.com>
2022-09-29 14:09:37 +0800

    init

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+//Target Date in Milliseconds
+const countdownDate = new Date("September 30, 2022 01:30:50").getTime();
 
-var days = new Date().getDay();
-var hours = new Date().getHours();
-var minutes = new Date().getMinutes();
-var seconds = new Date().getSeconds();
+
+
+let x = setInterval(() => {
+
+//Todays date in milliseconds
+const today = new Date().getTime();
+
+//Difference between target and today's date in milliseconds 
+let distance = countdownDate - today;
+
+let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+let seconds = Math.floor((distance % (1000 * 60 )) / 1000 );
 
 
 document.getElementById('days').innerHTML = days;
 document.getElementById('hours').innerHTML = hours;
 document.getElementById('minutes').innerHTML = minutes;
 document.getElementById('seconds').innerHTML = seconds;
+
+}, 1000);
+
+
+
